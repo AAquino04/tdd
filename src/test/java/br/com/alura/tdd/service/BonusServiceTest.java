f7c1e50a0152e90b929523132c642932359a45c0
Anderson Aquino <andersonaquino93@gmail.com>
2021-10-05 22:24:58 -0300

    implement BonusService test case 3

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -20,11 +20,19 @@ class BonusServiceTest {
 	}
 	
 	@Test
-	void bonusDeveriaSer10PorCentoDoSalario() {
+	void bonusDeveriaSerDezPorCentoDoSalario() {
 		BonusService service = new BonusService();
 		BigDecimal bonus = service.calcularBonus(new Funcionario("John Doe", LocalDate.now(), new BigDecimal("2500")));
 
 		assertEquals(new BigDecimal("250.00"), bonus);
+	}
+	
+	@Test
+	void bonusDeveriaSerDezPorCentoParaSalarioDeExatamente10Mil() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("John Doe", LocalDate.now(), new BigDecimal("10000")));
+
+		assertEquals(new BigDecimal("1000.00"), bonus);
 	}
 
 }
